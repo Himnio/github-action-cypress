8501ee90b4f1b20f8f95f5846ddc113c3804ab9b
Medhi <hmedhi338@apac.comcast.com>
2025-06-09 20:44:35 +0530

    testing

--- cypress.config.js
+++ cypress.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
   },
   reporter: 'mochawesome',
   reporterOptions: {
-    // reportDir: path.join(__dirname, 'cypress/reports'),
+    reportDir: path.join(__dirname, 'cypress/reports'),
     overwrite: false,
     html: true,
     json: true,
