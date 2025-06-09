a9602ad89cc01307c7e93ed64859536ea92cb59b
Medhi <hmedhi338@apac.comcast.com>
2025-06-09 16:45:39 +0530

    added the report type json and html

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+  e2e:{
+    setupNodeEvents(on, config){},
   },
-});
+  reporter: 'mochawesome',
+  reporterOptions: {
+    overwrite: false,
+    html: true,
+    json: true,
+  },
+})
